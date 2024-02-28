98ed812be94732c2f0c520222d75bc215c5a4722
Rob van der Linde <robvdl@gmail.com>
2024-02-29 08:55:17 +1300

    request: add samdb property to request
    This still requires authentication as well as a session backend configured, but has been tested with hardcoded credentials and works.

--- src/sambal/__init__.py
+++ src/sambal/__init__.py
@@ -1,9 +1,12 @@
 from pyramid.config import Configurator
 
+from .client import get_samdb
+
 
 with Configurator() as config:
     config.include("pyramid_jinja2")
     config.include("sambal.routes")
     config.add_jinja2_search_path("sambal:templates")
+    config.add_request_method(get_samdb, "samdb", property=True, reify=True)
     config.scan("sambal.views")
     app = config.make_wsgi_app()
---
+++ src/sambal/client.py
@@ -0,0 +1,45 @@
+from typing import Optional
+
+from samba.credentials import Credentials
+from samba.auth import system_session
+from samba.param import LoadParm
+from samba.samdb import SamDB
+
+
+def get_samdb(request) -> Optional[SamDB]:
+    """Returns a SamDB connection to be used via the request.samdb property.
+
+    :param request: Pyramid request object
+    :return: SamDB or None if no credentials in session
+    """
+    # Fetch credentials out of the session after user logs in.
+    # For this to be secure the session MUST be a backend session only.
+    host = request.session.get("samba.host")
+    realm = request.session.get("samba.realm")
+    username = request.session.get("samba.username")
+    password = request.session.get("samba.password")
+
+    # The host username and password are required, realm is optional.
+    # Needs to be double-checked if this is correct.
+    if host and username and password:
+        if host.startswith(("ldap://", "ldaps://")):
+            url = host
+        else:
+            url = f"ldap://{host}"
+
+        lp = LoadParm()
+        lp.load_default()
+
+        creds = Credentials()
+        creds.set_username(username)
+        creds.set_password(password)
+
+        if realm:
+            creds.set_realm(realm)
+
+        return SamDB(
+            url=url,
+            session_info=system_session(),
+            credentials=creds,
+            lp=lp,
+        )
