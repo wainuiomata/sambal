8eb2e6af4d19a1f7ee121fa82b2e751257ef87ae
Rob van der Linde <robvdl@gmail.com>
2024-02-28 23:31:37 +1300

    config: set jinja2 search path to sambal:templates

--- src/sambal/__init__.py
+++ src/sambal/__init__.py
@@ -4,5 +4,6 @@ from pyramid.config import Configurator
 with Configurator() as config:
     config.include("pyramid_jinja2")
     config.include("sambal.routes")
+    config.add_jinja2_search_path("sambal:templates")
     config.scan("sambal.views")
     app = config.make_wsgi_app()
