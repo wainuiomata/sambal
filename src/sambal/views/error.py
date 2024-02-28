e4566833f621b5cf719b0136bc6999cd5eeabb7f
Rob van der Linde <robvdl@gmail.com>
2024-02-29 07:25:22 +1300

    views: no need to end notfound view in _view

--- src/sambal/views/error.py
+++ src/sambal/views/error.py
@@ -2,6 +2,6 @@ from pyramid.view import notfound_view_config
 
 
 @notfound_view_config(renderer="404.jinja2")
-def notfound_view(request):
+def notfound(request):
     request.response.status = 404
     return {}
