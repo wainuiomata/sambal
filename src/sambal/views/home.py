bd1d9a5270b04b1dad7f3b6f23c6db55faa2af60
Rob van der Linde <robvdl@gmail.com>
2024-03-01 06:45:09 +1300

    views: Use Computer model instead for testing

--- src/sambal/views/home.py
+++ src/sambal/views/home.py
@@ -1,8 +1,8 @@
 from pyramid.view import view_config
-from samba.netcmd.domain.models import User
+from samba.netcmd.domain.models import Computer
 
 
 @view_config(route_name="home", renderer="home.jinja2")
 def home(request):
-    users = User.query(request.samdb)
-    return {"project": "sambal", "users": users}
+    computers = Computer.query(request.samdb)
+    return {"project": "sambal", "computers": computers}
