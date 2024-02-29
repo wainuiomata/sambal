3a4b4477e541fae4c62e52bb936d41e575695da0
Rob van der Linde <robvdl@gmail.com>
2024-02-29 22:27:29 +1300

    views: add sample query to home view for fetching User records
    It doesn't need to display these users in the jinja2 template, it's just there so that something is using the request.samdb property for testing purposes.
    
    Note that users is a Query object which is iterable, a class that can be used like a generator for returning records when looping over it.
    
    The fact that the import is still under "samba.netcmd.domain.models" is something I want to change to "samba.domain.models" in upstream Samba, as the models have nothing to do with the cli commands.

--- src/sambal/views/home.py
+++ src/sambal/views/home.py
@@ -1,6 +1,8 @@
 from pyramid.view import view_config
+from samba.netcmd.domain.models import User
 
 
 @view_config(route_name="home", renderer="home.jinja2")
 def home(request):
-    return {"project": "sambal"}
+    users = User.query(request.samdb)
+    return {"project": "sambal", "users": users}
