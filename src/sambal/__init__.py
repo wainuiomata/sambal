48cf3dfc75b0602363b3a58c4cd7c7d2bc27ce0a
Rob van der Linde <robvdl@gmail.com>
2024-03-02 21:13:58 +1300

    security: Upgrade to Pyramid 2.0 style SecurityPolicy
    * login and logout functions added to security.py have been tested
    * connect_samdb extracted to a function so it can be called from two places
    * csrf setup as well
    * request.user property removed and move to Pyramid's official version which is request.identity

--- src/sambal/__init__.py
+++ src/sambal/__init__.py
@@ -1,25 +1,18 @@
-from pyramid.authentication import AuthTktAuthenticationPolicy
 from pyramid.config import Configurator
+from pyramid.csrf import CookieCSRFStoragePolicy
 
 from .client import get_samdb
-from .security import get_authenticated_user, groupfinder
+from .security import SambalSecurityPolicy
 from .settings import SETTINGS
 
-authn_policy = AuthTktAuthenticationPolicy(
-    SETTINGS["authn.secret"],
-    http_only=True,
-    secure=False,
-    callback=groupfinder,
-    hashalg="sha512",
-    samesite="Strict",
-)
-
-with Configurator(authentication_policy=authn_policy, settings=SETTINGS) as config:
+with Configurator(settings=SETTINGS) as config:
     config.include("pyramid_jinja2")
     config.include("sambal.routes")
     config.include("pyramid_session_redis")
+    config.set_csrf_storage_policy(CookieCSRFStoragePolicy())
+    config.set_default_csrf_options(require_csrf=True)
+    config.set_security_policy(SambalSecurityPolicy(SETTINGS["sambal.secret"]))
     config.add_jinja2_search_path("sambal:templates")
     config.add_request_method(get_samdb, "samdb", property=True, reify=True)
-    config.add_request_method(get_authenticated_user, "user", property=True, reify=True)
     config.scan("sambal.views")
     app = config.make_wsgi_app()
--- src/sambal/security.py
+++ src/sambal/security.py
@@ -1,11 +1,64 @@
 from typing import Optional
 
+from pyramid.authentication import AuthTktCookieHelper
+from pyramid.authorization import ACLHelper, Authenticated, Everyone
+from pyramid.interfaces import ISecurityPolicy
+from pyramid.security import forget, remember
 from samba.netcmd.domain.models import User
+from zope.interface import implementer
+
+from .client import connect_samdb
 
 
-def get_authenticated_user(request) -> Optional[User]:
-    """Returns the authenticated Samba User object."""
-    return User.get(request.samdb, object_sid=request.samdb.connecting_user_sid)
+@implementer(ISecurityPolicy)
+class SambalSecurityPolicy:
+    def __init__(self, secret):
+        self.authtkt = AuthTktCookieHelper(secret=secret)
+
+    def identity(self, request) -> Optional[User]:
+        object_sid = self.authenticated_userid(request)
+        if object_sid:
+            return User.get(request.samdb, object_sid=object_sid)
+
+    def authenticated_userid(self, request) -> Optional[str]:
+        if request.samdb:
+            return request.samdb.connecting_user_sid
+
+    def permits(self, request, context, permission):
+        # Use the identity to build a set of principals.
+        principals = {Everyone}
+        identity = request.identity
+        if identity is not None:
+            principals.add(Authenticated)
+
+        # Pass them to the ACLHelper to determine allowed/denied.
+        return ACLHelper().permits(context, principals, permission)
+
+    def remember(self, request, userid, **kwargs):
+        return self.authtkt.remember(request, userid, **kwargs)
+
+    def forget(self, request, **kwargs):
+        return self.authtkt.forget(request, **kwargs)
+
+
+def login(request, username, password, host, realm):
+    """Log into server and put credentials in session on success only."""
+    samdb = connect_samdb(username, password, host, realm)
+    if samdb and (user_sid := samdb.connecting_user_sid):
+        request.session["samba.username"] = username
+        request.session["samba.password"] = password
+        request.session["samba.host"] = host
+        request.session["samba.realm"] = realm
+        return remember(request, user_sid)
+
+
+def logout(request):
+    """Log the user out and delete credentials from the session."""
+    del request.session["samba.username"]
+    del request.session["samba.password"]
+    del request.session["samba.host"]
+    del request.session["samba.realm"]
+    return forget(request)
 
 
 def groupfinder(username, request):
