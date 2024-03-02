d401967710ad354bca2475892c27cf3c02d4df03
Rob van der Linde <robvdl@gmail.com>
2024-03-02 22:19:26 +1300

    config: rename env var to SAMBAL_SECRET

--- src/sambal/settings.py
+++ src/sambal/settings.py
@@ -4,9 +4,9 @@ import os
 # Pyramid settings are traditionally loaded via PasteDeploy ini file.
 # With this project we went a different way with env vars.
 SETTINGS = {
-    "sambal.secret": os.getenv("SAMBAL_SESSION_SECRET"),
+    "sambal.secret": os.getenv("SAMBAL_SECRET"),
     "redis.sessions.url": os.getenv("SAMBAL_REDIS_URL"),
-    "redis.sessions.secret": os.getenv("SAMBAL_SESSION_SECRET"),
+    "redis.sessions.secret": os.getenv("SAMBAL_SECRET"),
     "redis.sessions.serialize": lambda s: json.dumps(s).encode("utf-8"),
     "redis.sessions.deserialize": lambda s: json.loads(s.decode("utf-8")),
     "redis.sessions.cookie_httponly": True,
