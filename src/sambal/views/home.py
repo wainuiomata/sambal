012ae1d36b3d1431e68cffa8a1dea23f414c7838
Rob van der Linde <robvdl@gmail.com>
2024-03-12 09:54:57 +1300

    config: home view should have been deleted in traversal PR

--- src/sambal/views/home.py
+++
@@ -1,8 +0,0 @@
-from pyramid.view import view_config
-from samba.netcmd.domain.models import Computer
-
-
-@view_config(route_name="home", renderer="home.jinja2", permission="read")
-def home(request):
-    computers = Computer.query(request.samdb)
-    return {"project": "sambal", "computers": computers, "user": request.identity}
