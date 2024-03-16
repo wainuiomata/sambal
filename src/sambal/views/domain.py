905bcd5965a715b19248a73389e0fca7e028a402
Rob van der Linde <robvdl@gmail.com>
2024-03-16 23:46:40 +1300

    views: traversal views don't show infinite nested data
    the flatten function will likely get moved after
    
    closes #43

--- src/sambal/views/domain.py
+++ src/sambal/views/domain.py
@@ -1,6 +1,19 @@
 from pyramid.view import view_config
 
-from sambal.resources import Resource, RootFactory
+from sambal.resources import ContainerResource, Resource, RootFactory
+
+
+def flatten(resource: Resource) -> dict:
+    """Remove nested containers at the view level.
+
+    This can't easily be done at the Resource level otherwise it stops
+    traversal completely.
+    """
+    # Filter children of containers to stop it returning infinite nested data.
+    if isinstance(resource, ContainerResource):
+        return {k: obj for k, obj in resource.items() if not isinstance(obj, Resource)}
+
+    return resource
 
 
 @view_config(context=Resource, permission="read", renderer="json")
@@ -11,4 +24,4 @@ def resource_view(context, request):
     For this to work a custom JSON encoder is used to deal with the
     various objects that aren't JSON encode-able out of the box.
     """
-    return context
+    return {k: flatten(obj) for k, obj in context.items() if isinstance(obj, Resource)}
